ad9eda8f90eef06c6aa05ce1247ef7dec75e3c71
Kavita Patel <kpatel@miracosta.edu>
2025-08-03 20:03:03 -0700

    Create GUI_Modules.py

---
+++ Lab_05/GUI_Modules.py
@@ -0,0 +1,42 @@
+
+'''
+
+Lab 05 - Programming in Python: Modules & GUIs
+
+Description: Travel from the 1970's into the modern times with tkinter. You are going to create a GUI! This will be a good beggining taste of what is possible with GUI's and you can modify this GUI with anything you want and keep updating it as you learn. I hope you enjoy it!
+
+Developer Name(s): 
+
+Date: 
+
+'''
+##########################################
+# IMPORTS:
+#   modules needed for program
+##########################################
+# This is where you need to import tkinter
+
+
+
+
+##########################################
+# FUNCTIONS:
+#   functions useful to main program
+##########################################
+
+
+def binary():
+	user_input = int(input("Enter a number: ")) #retrieves input from user
+	answer = bin(user_input)[2:] #converts to binary
+	print(answer) #displays answer
+	
+	
+
+##########################################
+# MAIN PROGRAM:
+#   beginning of running program
+##########################################
+
+
+binary() #DELETE THIS AFTER YOU TEST IT
+
